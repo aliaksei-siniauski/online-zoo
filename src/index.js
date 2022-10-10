861f24b0f5d635fdea6120603345ad993f63cc34
github-username <mail@gmail.com>
2022-10-10 23:46:31 +0200

    feat: add modal function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import { toggleMenu } from "./js/BurgerMenu.js";
 import { startTestimonialsSlider } from "./js/TestimonialSlider.js";
+import { addTestimonialHandler } from "./js/TestimonialModal.js";
