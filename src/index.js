d42ef0f92d41ed10c8ef0b6843f472e11fb6788e
github-username <mail@gmail.com>
2022-10-12 02:43:44 +0200

    refactor: create new alert message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { toggleMenu } from "./js/BurgerMenu.js";
 import { startTestimonialsSlider } from "./js/TestimonialSlider.js";
-import {
-  addPricesClickHandler,
-  addProgressRollersClickHandler,
-} from "./js/TestimonialModal.js";
+import { addTestimonialHandler } from "./js/TestimonialModal.js";
+
+alert(
+  "Вітаю!. Калі ласка, калі вам будзе нескладана, не маг лі бы вы праверыць маю парацу ў сераду вечарам. Хацелась бы дарабіць слайдар картак з жывёламі. Дзякуй Вялікі!!"
+);
